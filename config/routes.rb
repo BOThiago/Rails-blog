240b07d3c1f7e2d1e2b1fbdb82e49219b152dcbf
BOThiago <thiago.barreto.oliveira22@gmail.com>
2024-08-05 00:17:14 -0300

    feat: index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
